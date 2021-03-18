6da578157fde8d271329b06502498263edc46ef9
Karolis Bliodnieks <karolis@scale3c.com>
2021-03-18 22:37:09 +0200

    refactor: DisplayList

--- public/handlers.js
+++ public/handlers.js
@@ -5,3 +5,11 @@ const handleSort = (headers, el) => {
   el.target.classList.add("sort");
   sortTable(el.target);
 };
+
+const handleItemClick = (el, listElement) => {
+  for (let index = 0; index < listElement.rows.length; index++) {
+    listElement.rows[index].classList.remove("active");
+  }
+  el.path[1].classList.add("active");
+  selection.innerText = el.target.innerText;
+};
